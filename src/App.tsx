832b34060ad9f0d99a4497e8c3dce9132e3ed96d
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-27 22:14:39 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as dashboard from '@wix/dashboard';
+import { Card, Page, Text } from '@wix/design-system';
+import '@wix/design-system/styles.global.css';
+import * as sdk from '@wix/sdk';
+import { useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    // @ts-ignore
+    window.app_sdk = sdk;
+    // @ts-ignore
+    window.app_dashboard = dashboard;
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Page height="100vh">
+      <Page.Header title="Dashboard App" />
+      <Page.Content>
+        <Card>
+          <Card.Header title="Search parameters" />
+          <Card.Divider />
+          <Card.Content>
+            <Text><pre>{window.location.search}</pre></Text>
+          </Card.Content>
+        </Card>
+      </Page.Content>
+    </Page>
   )
 }
 
