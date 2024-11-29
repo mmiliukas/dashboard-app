a7d966b926a93de5dfd39eb05779f981e2c60ed2
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-29 15:06:13 +0200

    .

--- src/components/subscriptions.tsx
+++ src/components/subscriptions.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from 'react';
-import { useFetch } from '../hooks/useFetch';
+import { Button, Card, FormField, Input } from '@wix/design-system';
+import { useCallback, useEffect, useState } from 'react';
+import { useFetch, usePost } from '../hooks/useFetch';
 
 export function Subscriptions() {
     const fetch = useFetch();
+    const post = usePost();
+
+    const [email, setEmail] = useState<string>("");
 
     useEffect(() => {
         fetch('/subscriptions').then(result => {
@@ -12,27 +16,47 @@ export function Subscriptions() {
         });
     }, [fetch]);
 
-    return <div>Subscriptions</div>
-    // return (
-    //     <Table skin="standard" data={[]} columns={[
-    //         {
-    //             title: 'Image',
-    //             render: (row) => (
-    //                 row.firstImageUrl && <Image width="64px" src={row.firstImageUrl} />
-    //             ),
-    //         },
-    //         {
-    //             title: 'Title',
-    //             render: (row) => <TextButton size="small" onClick={() => openCampaign(row.campaignId!)}>{row.title}</TextButton>,
-    //         },
-    //         {
-    //             title: 'Status',
-    //             render: (row) => <Badge size="small">{row.status}</Badge>
-    //         },
-    //         {
-    //             title: 'Created At',
-    //             render: (row) => row.dateCreated?.toLocaleString(),
-    //         },
-    //     ]} />
-    // );
+    const subscribe = useCallback(() => {
+        post('/subscriptions', { email }).then(result => {
+            console.log(result);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, [fetch, email]);
+
+    return (
+        <Card>
+            <Card.Header title="Subscriptions" />
+            <Card.Divider />
+            <Card.Content>
+                <FormField label="Email">
+                    <Input value={email} onChange={e => setEmail(e.target.value)} />
+                </FormField>"
+                <Button size="small" priority="secondary" onClick={subscribe}>Subscribe</Button>
+            </Card.Content>
+        </Card>
+    );
 }
+// return (
+//     <Table skin="standard" data={[]} columns={[
+//         {
+//             title: 'Image',
+//             render: (row) => (
+//                 row.firstImageUrl && <Image width="64px" src={row.firstImageUrl} />
+//             ),
+//         },
+//         {
+//             title: 'Title',
+//             render: (row) => <TextButton size="small" onClick={() => openCampaign(row.campaignId!)}>{row.title}</TextButton>,
+//         },
+//         {
+//             title: 'Status',
+//             render: (row) => <Badge size="small">{row.status}</Badge>
+//         },
+//         {
+//             title: 'Created At',
+//             render: (row) => row.dateCreated?.toLocaleString(),
+//         },
+//     ]} />
+// );
+// }
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -6,6 +6,22 @@ export function useFetchInstance() {
     }, []);
 }
 
+export function usePost() {
+    const instance = useFetchInstance();
+
+    return useCallback((pathname: string, params: Record<string, string> = {}) => {
+        const fullUrl = new URL("https://dashboard-app-server-isy0.onrender.com");
+        fullUrl.pathname = pathname;
+
+        const search = new URLSearchParams();
+        search.set('instance', instance);
+
+        fullUrl.search = search.toString();
+
+        return fetch(fullUrl.toString(), { method: 'POST', body: JSON.stringify(params) }).then(result => result.json());
+    }, [instance]);
+}
+
 export function useFetch() {
     const instance = useFetchInstance();
 
