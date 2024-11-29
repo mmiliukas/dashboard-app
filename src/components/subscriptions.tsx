7f8c637780d93a2cea0a0a11eb253ce2b0b42d45
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-29 15:46:35 +0200

    .

--- src/components/subscriptions.tsx
+++ src/components/subscriptions.tsx
@@ -1,5 +1,5 @@
 import { useDashboard } from '@wix/dashboard-react';
-import { Button, Card, Cell, FormField, Input, Layout } from '@wix/design-system';
+import { Badge, Button, Card, Cell, FormField, Input, Layout, Table } from '@wix/design-system';
 import { useCallback, useEffect, useState } from 'react';
 import { useFetch, usePost } from '../hooks/useFetch';
 
@@ -9,10 +9,11 @@ export function Subscriptions() {
     const { showToast } = useDashboard();
 
     const [email, setEmail] = useState<string>("");
+    const [subscriptions, setSubscriptions] = useState([]);
 
     useEffect(() => {
         fetch('/subscriptions').then(result => {
-            console.log(result);
+            setSubscriptions(result.subscriptions || []);
         }).catch(error => {
             console.error(error);
         });
@@ -25,6 +26,11 @@ export function Subscriptions() {
                 message: 'Subscribed',
                 type: "success",
             });
+            fetch('/subscriptions').then(result => {
+                setSubscriptions(result.subscriptions || []);
+            }).catch(error => {
+                console.error(error);
+            });
         }).catch((error) => {
             console.error(error);
             showToast({
@@ -35,44 +41,41 @@ export function Subscriptions() {
     }, [fetch, email]);
 
     return (
-        <Card>
-            <Card.Header title="Subscriptions" />
-            <Card.Divider />
-            <Card.Content>
-                <Layout cols={1}>
-                    <Cell span={1}>
-                        <FormField label="Email">
-                            <Input value={email} onChange={e => setEmail(e.target.value)} />
-                        </FormField>
-                    </Cell>
-                    <Cell span={1}>
-                        <Button size="small" priority="secondary" onClick={subscribe}>Subscribe</Button>
-                    </Cell>
-                </Layout>
-            </Card.Content>
-        </Card>
+        <>
+            <Card>
+                <Card.Header title="Subscriptions" />
+                <Card.Divider />
+                <Card.Content>
+                    <Layout cols={1}>
+                        <Cell span={1}>
+                            <FormField label="Email">
+                                <Input value={email} onChange={e => setEmail(e.target.value)} />
+                            </FormField>
+                        </Cell>
+                        <Cell span={1}>
+                            <Button size="small" priority="secondary" onClick={subscribe}>Subscribe</Button>
+                        </Cell>
+                    </Layout>
+                </Card.Content>
+            </Card>
+            <Table skin="standard" data={subscriptions} columns={[
+                {
+                    title: 'Deliverability status',
+                    render: (row: any) => <Badge size="small">{row.deliverabilityStatus}</Badge>,
+                },
+                {
+                    title: 'Email',
+                    render: (row) => row.email,
+                },
+                {
+                    title: 'Subscription status',
+                    render: (row) => <Badge size="small">{row.subscriptionStatus}</Badge>
+                },
+                {
+                    title: 'Created At',
+                    render: (row) => row._createdDate,
+                },
+            ]} />
+        </>
     );
 }
-// return (
-//     <Table skin="standard" data={[]} columns={[
-//         {
-//             title: 'Image',
-//             render: (row) => (
-//                 row.firstImageUrl && <Image width="64px" src={row.firstImageUrl} />
-//             ),
-//         },
-//         {
-//             title: 'Title',
-//             render: (row) => <TextButton size="small" onClick={() => openCampaign(row.campaignId!)}>{row.title}</TextButton>,
-//         },
-//         {
-//             title: 'Status',
-//             render: (row) => <Badge size="small">{row.status}</Badge>
-//         },
-//         {
-//             title: 'Created At',
-//             render: (row) => row.dateCreated?.toLocaleString(),
-//         },
-//     ]} />
-// );
-// }
