486bd88b6ba1e2ca64126b516135fa5e874cbad3
Mantas Miliukas <mmiliukas@gmail.com>
2024-12-02 15:51:21 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        component: './src/component.ts'
+      },
+      output: {
+        entryFileNames: 'assets/[name].bundle.js',
+        chunkFileNames: 'assets/[name].chunk.js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
